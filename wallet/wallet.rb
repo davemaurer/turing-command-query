032fd2be88b38230e34df2331b62100285b0ea72
David Maurer <davemaurermail@gmail.com>
2016-10-24 00:15:25 -0600

    set up hash for determining amount of cents in the wallet.

--- wallet/wallet.rb
+++ wallet/wallet.rb
@@ -1,7 +1,13 @@
-class Wallet
-
-  def cents
-    0
+class Wallet < Array
+  def money
+    {
+      pennies: self.count(:penny),
+      nickels: self.count(:nickel),
+      quarters: self.count(:quarter),
+      dollars: self.count(:dollar),
+    }
   end
-
+  def cents
+    money.values.reduce(:+)
+  end
 end
